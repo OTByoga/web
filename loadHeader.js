29d6505ffcdd32d84bf0db3d6e8445a7bfa9cafa
Jocke <134770170+OTByoga@users.noreply.github.com>
2023-06-09 14:10:19 +0300

    Add files via upload

---
+++ loadHeader.js
@@ -0,0 +1,23 @@
+// loadHeader.js
+window.onload = function() {
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+
+            const menuItems = [
+                {name: "Home", link: "index.html"},
+                {name: "Yoga Apps", link: "yoga_apps.html"},
+                {name: "Backstage", link: "backstage.html"},
+                // Add more items as needed
+            ];
+
+            let menuHTML = "";
+
+            menuItems.forEach(item => {
+                menuHTML += `<a href="${item.link}">${item.name}</a>`;
+            });
+
+            document.getElementById('menu').innerHTML = menuHTML;
+        });
+};
