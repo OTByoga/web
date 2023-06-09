185fd162f5519ada2147a4a242ae0e15de1f03b0
Jocke <134770170+OTByoga@users.noreply.github.com>
2023-06-09 19:08:25 +0300

    Add files via upload

--- loadHeader.js
+++ loadHeader.js
@@ -1,5 +1,5 @@
 // loadHeader.js
-window.onload = function() {
+document.addEventListener('DOMContentLoaded', function() {
     fetch('header.html')
         .then(response => response.text())
         .then(data => {
@@ -21,4 +21,4 @@ window.onload = function() {
 
             document.getElementById('menu').innerHTML = menuHTML;
         });
-};
+});
